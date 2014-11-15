d881624ab27cb0e1f157cf184a7decf6e0df9cc3
Ryan Huber <ryan.huber@mac.com>
2014-11-15 00:55:17 -0500

    Adds compass task to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
+
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/*.js'];
 				//Can list script files individually in the order in which you want them processed
+var sassSources = ['components/sass/main.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -22,3 +25,13 @@ gulp.task('js', function() {
 	  .pipe(gulp.dest('builds/development/js'))
 });
 
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	  .pipe(compass({
+	  	sass: 'components/sass',
+	  	image: 'builds/development/images',
+	  	style: 'expanded'
+	  })
+	  	.on('error', gutil.log))
+	  .pipe(gulp.dest('builds/development/css'))
+});
