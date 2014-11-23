f50b50c6db01111be17482da51459322a50a3906
Ryan Huber <ryan.huber@mac.com>
2014-11-23 11:03:53 -0500

    updates gulpfile to send css to proper destination

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
+// dependency requirements
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
-
+// variables for sources
 var coffeeSources = ['components/coffee/*.coffee'];
-var jsSources = ['components/scripts/*.js'];
-				//Can list script files individually in the order in which you want them processed
+var jsSources = ['components/scripts/*.js'];  //Can list script files individually in the order in which you want them processed
 var sassSources = ['components/sass/main.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
+
+
+// tasks
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	  .pipe(connect.reload())
+});
+
+gulp.task('json', function() {  // remove this task if you are not using json files
+	gulp.src(jsonSources)
+	  .pipe(connect.reload())
+});
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -23,6 +38,7 @@ gulp.task('js', function() {
 	  .pipe(concat('script.js'))
 	  .pipe(browserify())
 	  .pipe(gulp.dest('builds/development/js'))
+	  .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -34,12 +50,22 @@ gulp.task('compass', function() {
 	  })
 	  .on('error', gutil.log))
 	  .pipe(gulp.dest('builds/development/css'))
+	  .pipe(connect.reload())
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
 });
 
 gulp.task('watch', function() {
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['html', 'coffee', 'js', 'compass', 'connect', 'watch']);
